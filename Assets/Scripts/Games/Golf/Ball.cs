49844ef700683f0bcf164da0a441cad717fd1eab
Filip Karyś <Filip.karys@gmail.com>
2024-05-28 15:42:40 +0200

    Hole plus FBX
    Prefaby mapy plus stworzona dziura i  FBX trafiania

--- Assets/Scripts/Games/Golf/Ball.cs
+++ Assets/Scripts/Games/Golf/Ball.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AdaptivePerformance.VisualScripting;
 
 public class Ball : MonoBehaviour
 {
     [Header("Referances")]
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private LineRenderer lr;
+    [SerializeField] private GameObject goalFx;
 
     [Header("Attributes")]
     [SerializeField] private float maxPower = 10f;
@@ -27,21 +29,29 @@ public class Ball : MonoBehaviour
         float distance = Vector2.Distance(transform.position, inputPos);
 
         if (Input.GetMouseButtonDown(0) && distance <= 0.5f) DragStart();
-        if (Input.GetMouseButton(0) && isDraging) DragChange();
+        if (Input.GetMouseButton(0) && isDraging) DragChange(inputPos);
         if (Input.GetMouseButtonUp(0) && isDraging) DragRelease(inputPos);
     }
 
     private void DragStart()
     {
         isDraging = true;
+        lr.positionCount = 2;
     }
 
-    private void DragChange() {}
+    private void DragChange(Vector2 pos) 
+    {
+        Vector2 dir = (Vector2)transform.position - pos;
+
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, (Vector2)transform.position + Vector2.ClampMagnitude((dir * power) / 2, maxPower / 2));
+    }
 
     private void DragRelease(Vector2 pos)
     {
         float distance = Vector2.Distance((Vector2)transform.position,pos);
         isDraging = false;
+        lr.positionCount = 0;
 
         if (distance < 1f) {
             return;
@@ -50,5 +60,30 @@ public class Ball : MonoBehaviour
         Vector2 dir = (Vector2)transform.position - pos;
         rb.velocity= Vector2.ClampMagnitude (dir * power, maxPower);
 
+    }
+
+    private void CheckWinState()
+    {
+        if (inHole) return;
+
+        if (rb.velocity.magnitude <= maxGoalSpeed)
+        {
+            inHole = true;
+
+            rb.velocity = Vector2.zero;
+            gameObject.SetActive(false);
+
+            GameObject fx = Instantiate(goalFx, transform.position, Quaternion.identity);
+            Destroy(fx, 2f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Goal") CheckWinState();
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Goal") CheckWinState();
     }
 }
