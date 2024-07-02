8fc43ad70a6e3b125787be66de47367597761c66
Kacper Markiewicz <kacpermarkiewicz@protonmail.com>
2024-07-02 14:55:29 +0200

    Ball behaviour fixed

--- Assets/AnimateBall.cs
+++ Assets/AnimateBall.cs
@@ -1,11 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimateBall : MonoBehaviour
 {
+    [SerializeField] private Scrollbar scrollRef;
     private void Update()
     {
         transform.Rotate(new Vector3(0, 0, 20*Time.deltaTime));
+        SetBallPosition();
+    }
+
+    private void SetBallPosition()
+    {
+        Debug.Log(GetComponent<RectTransform>().anchoredPosition);
+        float newValue;
+        if (scrollRef.value < 0)
+        {
+            newValue = 0;
+        }
+        else if (scrollRef.value > 1)
+        {
+            newValue = 1;
+        }
+        else
+        {
+            newValue = scrollRef.value;
+        }
+        GetComponent<RectTransform>().anchoredPosition = new Vector3(newValue * 900 - 450, -210, 0);
     }
 }
