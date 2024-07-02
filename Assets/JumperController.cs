8481448e8a5b0b33f42d1b232b930f058589fa40
Kacper Markiewicz <kacpermarkiewicz@protonmail.com>
2024-07-02 14:07:22 +0200

    Diving remastered

--- Assets/JumperController.cs
+++ Assets/JumperController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class JumperController : MonoBehaviour
 {
     public Animator animatorRef;
+    public Animator splashRef;
 
     public void GoodJump()
     {
         animatorRef.SetTrigger("GoodJump");
+        splashRef.SetTrigger("Splash");
     }
 
     public void BadJump() 
